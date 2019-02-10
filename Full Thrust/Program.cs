6d72380b49a9a51ae50a22259eafa4a49981e06c
MrMorham <g7058190@gmail.com>
2019-02-10 23:36:47 +0000

    added Ship Class

---
+++ Full Thrust/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Full_Thrust
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
